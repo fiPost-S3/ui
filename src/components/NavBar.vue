fd0520a093977a5005f7c76052723b968bd6c894
Robin de Witte <r.dewitte@student.fontys.nl>
2021-03-16 13:48:12 +0100

    navbar opzet maar assets zijn nog niet gemerged

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul id="topbar">
+            <li id="fontyslogo"><img></img></li>
+            <li><a id="logout"><img id="logoutbutton"></img></a></li>
+        </ul>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@import "./styling/main.css";
+#topbar{
+width: 100%;
+heigth: 10%;
+
+background-color: $fontys-purple;
+
+
+</style>

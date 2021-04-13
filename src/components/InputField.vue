5abf56a22e3aee6f6994b283125b563c857888c0
Shady^ <36726909+ShadyDL@users.noreply.github.com>
2021-04-13 12:28:20 +0200

    #203 register package responsive (#7)
    * Fixed responsiveness, package is reserved keyword fix, removed double inputs
    
    * Allow min width to be 150px
    
    Co-authored-by: Aron Heesakkers <aron.heesakkers@via.nl>

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,80 +1,87 @@
 <template>
-    <div class="input-container">
-        <p>{{label}}</p>
-        <input
-                v-if="valid"
-                class="input"
-                type="text"
-                v-model="text"
-                @change="changed"
-        />
-        <input
-                v-if="!valid"
-                class="input input-error"
-                type="text"
-                v-model="text"
-                @change="changed"
-        />
-    </div>
+  <div class="input-container">
+    <p class="header">{{ label }}</p>
+    <input
+      :class="cssClass"
+      type="text"
+      v-model="text"
+      @change="changed"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-    import {Options, Vue} from "vue-class-component";
-    import {Emit, Prop} from "vue-property-decorator";
+import { Options, Vue } from "vue-class-component";
+import { Emit, Prop } from "vue-property-decorator";
 
-    @Options({
-        emits: ['input-changed']
-    })
+@Options({
+  emits: ["input-changed"],
+})
+export default class InputField extends Vue {
+  @Prop() public label!: String;
+  @Prop() public input!: String;
+  @Prop() public color!: String;
+  @Prop() public valid: Boolean = true; // Default true
 
-    export default class InputField extends Vue {
-        @Prop() public label!: String;
-        @Prop() public input!: String;
-        @Prop() public color!: String;
-        @Prop() public valid: Boolean = true; // Default true
-
-        private text: String = this.input;
-
-        @Emit("input-changed")
-        changed() {
-            return this.text;
-        }
+  public get cssClass(): string {
+    if (this.valid) {
+      return "input";
     }
+    return "input input-error";
+  }
+
+  private text: String = this.input;
+
+  @Emit("input-changed")
+  changed() {
+    return this.text;
+  }
+}
 </script>
 
 <style scoped lang="scss">
-    @import "@/styling/main.scss";
+@import "@/styling/main.scss";
 
-    .input-container {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        width: 440px;
-        margin-bottom: 0.6rem;
-        margin-top: 0.6rem;
-        font-weight: normal;
-    }
+.input-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 440px;
+  margin-bottom: 0.6rem;
+  margin-top: 0.6rem;
+  font-weight: normal;
+  max-width: 100%;
+}
 
-    .input {
-        border-radius: 0.4rem;
-        border-width: 0;
-        background: $background-color;
-        padding-left: 0.8rem;
-        padding-right: 0.8rem;
-        width: 240px;
-        height: 2rem;
-        margin: auto 0;
-    }
+.header {
+  flex-basis: 100px;
+}
 
-    .input-error {
-        background: #ffc9cf;
-    }
+.input {
+  border-radius: 0.4rem;
+  border-width: 0;
+  background: $background-color;
+  padding-left: 0.8rem;
+  padding-right: 0.8rem;
+  width: 240px;
+  height: 2rem;
+  margin: auto 0;
+  max-width: 70%;
+  width: 200px;
+  min-width: 150px;
+}
 
-    .input:focus {
-        outline: none;
-        border: #999999 solid 2px;
-    }
+.input-error {
+  background: #ffc9cf;
+}
 
-    .input-error:focus {
-        border: #ff8ca1 solid 2px;
-    }
+.input:focus {
+  outline: none;
+  border: #999999 solid 2px;
+}
+
+.input-error:focus {
+  border: #ff8ca1 solid 2px;
+}
 </style>

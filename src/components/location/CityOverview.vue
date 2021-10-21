5c3b78e56288ee0b00b6436f1dc91480f4223362
StanvanBavel <73948369+StanvanBavel@users.noreply.github.com>
2021-10-21 10:12:10 +0200

    city overview

--- src/components/location/CityOverview.vue
+++ src/components/location/CityOverview.vue
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <template>
     <btn-back />
     <h1>Steden Overzicht</h1>
@@ -27,4 +28,35 @@ export default class CityOverview extends Vue {}
 
 <style scoped lang="scss">
 @import "@/styling/main.scss";
+=======
+<template>
+    <btn-back />
+    <h1>Steden Overzicht</h1>
+    <SearchContainerCityBuilding />
+    <CityOverviewTable/>
+    
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import BtnBack from "@/components/standardUi/BtnBack.vue";
+import CityOverviewTable from "@/components/location/CityOverviewTable.vue";
+import SearchContainerLocation from "@/components/SearchContainerLocation.vue";
+import SearchContainerCityBuilding from "@/components/SearchContainerCityBuilding.vue";
+import LocationOverviewTable from "@/components/location/LocationOverviewTable.vue";
+@Options({
+  components: {
+    BtnBack,
+    LocationOverviewTable,
+    CityOverviewTable,
+    SearchContainerLocation,
+    SearchContainerCityBuilding
+  },
+})
+export default class CityOverview extends Vue {}
+</script>
+
+<style scoped lang="scss">
+@import "@/styling/main.scss";
+>>>>>>> Stashed changes
 </style>

9a21b5df34351bbeccb42b803609c89a9293a4d5
Axelpieterman <axelxeon@gmail.com>
2021-11-17 10:48:00 +0100

    login form
    login form met errors toegevoegd

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,18 @@ import PakketOverzicht from "@/views/PakketOverzicht.vue";
 import PackagePage from "@/views/PackagePage.vue";
 import AddLocation from "@/views/AddLocation.vue";
 import LocationOverview from "@/views/LocationOverview.vue";
+import Login from "@/views/Login.vue";
 import CityOverview from "@/components/location/CityOverview.vue"; import BuildingOverview from "@/components/location/BuildingOverview.vue";
 const routes = [
     {
         path: "/registratie",
         name: "RegisterPackage",
         component: RegisterPackage,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
     },
     {
         path: "/",
